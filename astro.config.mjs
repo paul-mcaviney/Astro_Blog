a309e1f514f9595c282bc7803643c43b7fe6582a
paul-mcaviney <paul.mcaviney@protonmail.com>
2024-01-29 10:59:30 -0700

    Installed RSS package

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://paulies-astro-blog.netlify.app/",
   vite: {
     server: {
       watch: {
