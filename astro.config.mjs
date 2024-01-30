0bd879164701aa742b835a5a1deee18affc50e0f
paul-mcaviney <paul.mcaviney@protonmail.com>
2024-01-29 18:13:59 -0700

    Made a theme toggle button

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://paulies-astro-blog.netlify.app/",
   vite: {
     server: {
       watch: {
-        usePolling: true,
-      },
-    },
+        usePolling: true
+      }
+    }
   },
+  integrations: [preact()]
 });
