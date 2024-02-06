c16d45738432b9038d7ad38a12df8d9735faaef1
paul-mcaviney <paul.mcaviney@protonmail.com>
2024-02-06 12:27:48 -0700

    Testing GraphQL for headless WP

---
+++ src/graphql/wordpressQuery.ts
@@ -0,0 +1,23 @@
+interface gqlParams {
+  query: string;
+  variables?: object;
+}
+
+export async function wpquery({ query, variables = {} }: gqlParams) {
+  const response = await fetch("https://gogogame.dev/graphql.", {
+    method: "post",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify({
+      query,
+      variables,
+    }),
+  });
+
+  if (!response.ok) {
+    console.error(response);
+    return {};
+  }
+
+  const { data } = await response.json();
+  return data;
+}
